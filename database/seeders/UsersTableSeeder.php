9981a3b7fb58696dcc40655fe62d77da2fec34dc
namdeptrai69 <tuannam2505@gmail.com>
2020-12-03 20:57:56 +0700

    seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,16 +14,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->truncate();
-        for ($i=0;$i<30;$i++){
+        // DB::table('users')->truncate();
+        // for ($i=0;$i<30;$i++){
             DB::table('users')->insert([
                 'name' => 'admin',
                 'phone' => '1234567890',
-                'address' => '1234567890',
-                'role' => '1',
-                'email' => $i.'admin@gmail.com',
+                'address' => 'Ha noi',
+                'postion' => 2,
+                'status' => 1,
+                'email' => 'admin@gmail.com',
                 'password' => bcrypt('123456'),
             ]);
-        }
+        // }
     }
 }
